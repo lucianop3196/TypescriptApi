458b89dbba8e4d29a9891a297b3ee32eca7477eb
Luciano Pennacchioni <luciano.p3196@gmail.com>
2022-03-17 00:06:39 -0300

    First commit

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+import express, {Application, Request, Response, NextFunction} from 'express'; //Application, type from express
+import config from './lib/config';
+import cors from "cors";
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import routes from './routes/index';
+
+const app: Application = express();
+app.use(express.urlencoded({extended: true, limit: '50mb'})); //middleware
+app.use(express.json({limit: '50mb'}));
+app.use(cookieParser());
+app.use(morgan('dev'));
+
+app.use(
+	cors({
+		origin: config.cors,
+		credentials: true,
+		methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+		allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept'],
+	})
+);
+
+interface error { //interfaces fill the role of naming types
+	status: number;
+	message: string;
+}
+app.use((err: error, req: Request, res: Response, next: NextFunction) => {
+	// eslint-disable-line no-unused-vars
+	const status = err.status || 500;
+	const message = err.message || err;
+	console.error(err);
+	res.status(status).send(message);
+});
+
+app.use('/api', routes);
+
+export default app;
